54d3d5fe08c9edf5ecb13b4f66144fb5f0dd2459
LMAP313069 <luis031396@gmail.com>
2021-11-08 14:01:21 -0800

    prueba

---
+++ Go Place Conductor/Go Place Conductor/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Go Place Conductor
+//
+//  Created by BigSur on 4/10/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBAction func tapToCloseKeyboard(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+}
+
+
+
+extension LoginViewController {
+    
+  
+    
+}
